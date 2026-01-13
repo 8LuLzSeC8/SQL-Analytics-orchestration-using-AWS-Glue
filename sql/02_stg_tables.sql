62a760e53b7d0cfb4dba367aebcf1e68bc16664f
8LuLzSeC8 <tejaganesh5@gmail.com>
2026-01-13 18:04:02 -0500

    feat: add stg_trips_enriched staging table

---
+++ sql/02_stg_tables.sql
@@ -0,0 +1,35 @@
+BEGIN;
+
+CREATE SCHEMA IF NOT EXISTS stg;
+
+CREATE TABLE IF NOT EXISTS stg.stg_trips_enriched (
+  vendorid               integer,
+  tpep_pickup_datetime   timestamp,
+  tpep_dropoff_datetime  timestamp,
+  passenger_count        bigint,
+  trip_distance          double precision,
+  ratecodeid             bigint,
+  store_and_fwd_flag     text,
+  pulocationid           integer,
+  dolocationid           integer,
+  payment_type           bigint,
+  fare_amount            double precision,
+  extra                  double precision,
+  mta_tax                double precision,
+  tip_amount             double precision,
+  tolls_amount           double precision,
+  improvement_surcharge  double precision,
+  total_amount           double precision,
+  congestion_surcharge   double precision,
+  airport_fee            double precision,
+  cbd_congestion_fee     double precision,
+  pu_borough             text,
+  pu_zone                text,
+  pu_servicezone         text,
+  do_borough             text,
+  do_zone                text,
+  do_servicezone         text,
+  loaded_at              timestamptz NOT NULL DEFAULT now()
+);
+
+COMMIT;
